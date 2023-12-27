23a56b9f442c0dcb9034d3a60e5040dc63af8f93
liushao1542 <45199666+liushao1542@users.noreply.github.com>
2023-12-27 19:28:32 +0800

    Update en.ts

--- app/locales/en.ts
+++ app/locales/en.ts
@@ -163,12 +163,12 @@ const en: LocaleType = {
     },
 
     Update: {
-      Version: (x: string) => `Version: ${x}`,
-      IsLatest: "Latest version",
-      CheckUpdate: "Check Update",
-      IsChecking: "Checking update...",
-      FoundUpdate: (x: string) => `Found new version: ${x}`,
-      GoToUpdate: "Update",
+      Version: (x: string) => ``,
+      IsLatest: "",
+      CheckUpdate: "",
+      IsChecking: "",
+      FoundUpdate: (x: string) => ``,
+      GoToUpdate: "",
     },
     SendKey: "Send Key",
     Theme: "Theme",
