58b600ebae9a02214a94c1e32acbd2eff2e069c5
liushao1542 <45199666+liushao1542@users.noreply.github.com>
2023-12-27 19:09:06 +0800

    Update exporter.tsx

--- app/components/exporter.tsx
+++ app/components/exporter.tsx
@@ -538,9 +538,9 @@ export function ImagePreviewer(props: {
           </div>
 
           <div>
-            <div className={styles["main-title"]}>NextChat</div>
+            <div className={styles["main-title"]}>小路-ChatGPT</div>
             <div className={styles["sub-title"]}>
-              github.com/Yidadaa/ChatGPT-Next-Web
+              http://one.shmh.top
             </div>
             <div className={styles["icons"]}>
               <ExportAvatar avatar={config.avatar} />
