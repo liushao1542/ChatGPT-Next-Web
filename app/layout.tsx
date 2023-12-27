3ac202c5d1edb8f893a8ed1c7237dc5bfc764e25
liushao1542 <45199666+liushao1542@users.noreply.github.com>
2023-12-27 18:51:04 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ export const metadata: Metadata = {
     { media: "(prefers-color-scheme: dark)", color: "#151515" },
   ],
   appleWebApp: {
-    title: "NextChat",
+    title: "小路-ChatGPT",
     statusBarStyle: "default",
   },
 };
